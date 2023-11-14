e7372aeded524b94ea4e13e90cb87865c0b02290
12 <12@DESKTOP-NK7HEP0>
2023-11-14 13:28:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите текст");
+string[] tehst;
+string text = Console.ReadLine();
+tehst = text.Split(' ');
+Console.WriteLine("Количество слов");
+Console.WriteLine(tehst.Length);
